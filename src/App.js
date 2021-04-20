a25163b2b8d7c783be288076e6dbd6d9857bf2c5
Burkes321 <sam.burke99@gmail.com>
2021-04-20 12:26:40 +0200

    navbar done, must change anchor classnames as new sections are implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import NavBar from './components/NavBar';
 
 function App() {
-  
+  return(
+      <div className='app'>
+        <NavBar/>
+      </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <a href=".navbar">About me</a>
+            <a href=".navbar">My Skills</a>
+            <a href=".navbar">My Projects</a>
+            <a href=".navbar">Contact</a>
+        </div>
+    )
+}
+
+export default NavBar
