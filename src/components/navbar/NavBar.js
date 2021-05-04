c564931fea9714d0187def598363d5b39798fd2f
Samuel Burke <sam.burke99@gmail.com>
2021-05-04 19:10:29 +0100

    skills section reworked

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <div className='navbar'>
             <a href="about-me">About me</a>
-            <a href=".navbar">My Skills</a>
+            <a href="skills">My Skills</a>
             <a href=".navbar">My Projects</a>
             <a href=".navbar">Contact</a>
         </div>
