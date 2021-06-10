7dfe90e9dbf277fdc373ce811d22ce2ea4e0c813
Burkes321 <sam.burke99@gmail.com>
2021-06-10 22:51:16 +0200

    contact form styling fixed

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,7 +3,7 @@ import emailjs from 'emailjs-com';
 
 const Contact = () => {
 
-    const sendEmail = (e) => { 
+    const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs.sendForm('XFGHYT443', 'template_qyafjnt', e.target, 'user_0zkY31B8Cd5phIOfpR1Jj')
@@ -19,20 +19,20 @@ const Contact = () => {
             <h1>Contact Me</h1>
             <form className="contact-form" onSubmit={sendEmail}>
                 <div>
-                    <label>Name</label>
-                    <input type="text" name="user_name"/>
+                    <label>Your Name</label>
+                    <input type="text" name="user_name" />
                 </div>
                 <div>
                     <label>Subject</label>
-                    <input type="text" name="subject"/>
+                    <input type="text" name="subject" />
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="email" name="user_email"/>
+                    <input type="email" name="user_email" />
                 </div>
                 <div>
                     <label>Message</label>
-                    <textarea name="message"/> 
+                    <textarea name="message" />
                 </div>
                 <button action='submit'>Send!</button>
             </form>
